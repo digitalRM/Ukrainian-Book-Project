84fbe7631c96f6a4df6c9b14a32261ea7e4135bb
Ruslan <70782025+digitalRM@users.noreply.github.com>
2023-12-20 14:38:59 -0800

    better img

--- components/section2.jsx
+++ components/section2.jsx
@@ -11,7 +11,7 @@ const people = [
     name: 'Lindsay Walton',
     role: 'Front-end Developer',
     imageUrl:
-      'https://images.unsplash.com/photo-1682686580950-960d1d513532?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1702971916897-8f510b82d482?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     twitterUrl: '#',
     linkedinUrl: '#',
   },
