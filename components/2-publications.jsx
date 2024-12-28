6ee5b38ce2424f0feb1825986ddb54095d556fdb
Ruslan Mukhamedvaleev <r1554899@gmail.com>
2024-12-27 23:16:02 -0800

    Update ACH publication

--- components/2-publications.jsx
+++ components/2-publications.jsx
@@ -89,6 +89,34 @@ export default function Publications() {
                 />
               </span>
             </a>
+            <a href="https://www.figma.com/design/a587LZy952mJq3tZyj4hvF/Mapping-Ukrainian-collections-in-U.S.-public-libraries%E2%80%8B?node-id=0%3A1&t=zeTExXcoyamPbZZY-1">
+              <div className="aspect-h-2 aspect-w-3 w-full overflow-hidden rounded-lg h-[330.41px]">
+                <img
+                  src="./ach.png"
+                  alt="Poster highlighting early findings about the availability of Ukrainian language books in libraries across the United States."
+                  className="h-full w-full object-cover object-center grayscale hover:grayscale-0"
+                />
+              </div>
+              <p className="mt-8 text-base text-[#b3b2a8]">
+                <span className={hedvigLettersSans.className}>
+                  This poster highlights our early findings about the
+                  availability of Ukrainian language books in libraries across
+                  the United States. Presented at the 2024 Washington State
+                  University Academic Showcase and the 2024 WSU Everett Research
+                  Showcase.
+                </span>
+              </p>
+              <span className="mt-4 block text-md font-semibold hover:text-[#f3f1e9]/70 text-[#f3f1e9]/90 w-fit">
+                <span className={hedvigLettersSans.className}>
+                  View Poster{" "}
+                </span>
+                <ArrowUpRightSquareIcon
+                  aria-hidden="true"
+                  className="inline"
+                  size={16}
+                />
+              </span>
+            </a>
           </div>
         </section>
       </div>
